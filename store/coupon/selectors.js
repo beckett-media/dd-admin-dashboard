06745b7f49d0fae136c3a133e69a59c3641523d4
diode02 <qaskhan02@gmail.com>
2022-03-16 19:09:01 +0500

    selector-fix

--- store/coupon/selectors.js
+++ store/coupon/selectors.js
@@ -1,16 +1,13 @@
 const getData = (state) => state.coupon;
 
 export const getFilters = (state) => getData(state).filters;
-export const getPagination = (state) => {
-  console.log(getData(state));
-  getData(state).pagination;
-};
+export const getPagination = (state) => getData(state).pagination;
 
 // export const getProductCreateLoading = (state) => getData(state).isProductCreateLoading;
 
 export const getCurrentCouponList = (state) => {
-//   const  pagination  = getPagination(state);
-  const list = getData(state).list[getData.pagination.page];
+  const { page } = getPagination(state);
+  const list = getData(state).list[page];
   return list || [];
 };
 
