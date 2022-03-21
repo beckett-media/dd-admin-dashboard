aabf9c894029ad1f52590e37551a4fc5300c1261
diode02 <qaskhan02@gmail.com>
2022-03-21 23:48:33 +0500

    S Code cleanup

--- store/coupon/selectors.js
+++ store/coupon/selectors.js
@@ -1,13 +1,9 @@
 const getData = (state) => state.coupon;
-console.log(getData,"list");
 
 export const getFilters = (state) => getData(state).filters;
 export const getPagination = (state) => getData(state).pagination;
 
-// export const getProductCreateLoading = (state) => getData(state).isProductCreateLoading;
-
 export const getCurrentCouponList = (state) => {
-  console.log(state,);
   const { page } = getPagination(state);
   const list = getData(state).list[page];
   return list || [];
@@ -15,7 +11,3 @@ export const getCurrentCouponList = (state) => {
 
 export const getCouponDeleteLoader = (state) =>
   getData(state).isCouponDeleteLoading;
-
-// export const getSelectedProduct = (state) => getData(state).selectedProduct;
-
-// export const getEditLoader = (state) => getData(state).isEditLoading;
